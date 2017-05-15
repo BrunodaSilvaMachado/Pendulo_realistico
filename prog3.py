da89566ef51cf93a02c78db1c6a8fb2b7387e59f
caique <caique>
2017-05-15 20:50:49 -0300

    prog3.py-versao final

--- prog3.py
+++ prog3.py
@@ -7,7 +7,7 @@ import getopt,sys
 _fileName = "output.dat"
 # end
 
-def grafics(_tlist,_dlist,_imagName,_title,_xLabel,_yLabel):
+def grafics(_tlist,_dlist,_imagName,_title,_legend,_xLabel,_yLabel):
 	print 'processando...'
 	plt.figure(figsize=(5,4),dpi=96)
 	
@@ -27,26 +27,30 @@ def grafics(_tlist,_dlist,_imagName,_title,_xLabel,_yLabel):
 	
 	plt.grid()
 	plt.title(_title,fontsize = 14)
-	plt.plot(_tlist,_dlist,'k-',linewidth = 2)
-	plt.savefig(_fileName+"."+_imagName+'.png',dpi=96)
+	plt.plot(_tlist,_dlist,'k-',linewidth = 2,label = _legend)
+	plt.legend(loc = 'upper right')
+	plt.savefig(_fileName.replace('.dat','_')+_imagName+'.png',dpi=96)
 
 def main():
 	x = []
 	v = []
 	t = []
+	e = []
 	
 	arq1 = open(_fileName,'r')
 	
 	for line in arq1:
-		a,b,c = line.split()
+		a,b,c,d = line.split()
 		x.append(float(b))
 		v.append(float(c))
+		e.append(float(d))
 		t.append(float(a))
 	arq1.close()
 	
-	grafics(t,x,'image1',r'Pendulo-posi\c{c}\~{a}o',r'\textit{tempo}(s)',r'\textit{posi\c{c}\~{a}o} (m)')
-	grafics(t,v,'image2',r'Pendulo-velocidade',r'\textit{tempo}(s)',r'\textit{velocidade}(m/s)')
-	grafics(x,v,'image3',r'Espa\c{c}o de fases',r'\raggedright{\textit{posi\c{c}\~{a}o} (m)}',r'\raggedleft{\textit{velocidade}(m/s)}')
+	grafics(t,x,'image1',r'Pendulo',r"posi\c{c}\~{a}o",r'\textit{tempo}(s)',r'\textit{posi\c{c}\~{a}o} (m)')
+	grafics(t,v,'image2',r'Pendulo',"velocidade",r'\textit{tempo}(s)',r'\textit{velocidade}(m/s)')
+	grafics(x,v,'image3',r'Espa\c{c}o de fases',r"X/V",r'\raggedright{\textit{posi\c{c}\~{a}o} (m)}',r'\raggedleft{\textit{velocidade}(m/s)}')
+	grafics(t,e,'image4',r'Pendulo',"Energia",r'\textit{tempo}(s)',r'\textit{Energia} (j)')
 		
 opcao,valor = getopt.getopt(sys.argv[1:],'f: ')
 
